7efa52fc0a9d4978648f89d758c5de7895753da5
Lishakar <lishakarj@gmail.com>
2024-06-23 19:19:08 +0530

    Initial Commit

---
+++ src/main/java/com/splitwise/Expense_Service/ExpenseServiceApplication_1.java
@@ -0,0 +1,13 @@
+package com.splitwise.Expense_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseServiceApplication.class, args);
+	}
+
+}
