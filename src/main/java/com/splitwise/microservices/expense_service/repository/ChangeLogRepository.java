81448f2922a995a9ab7d8c00e361b8e669207abd
Lishakar <lishakarj@gmail.com>
2024-07-16 22:51:22 +0530

    Activity moved to Activity Microservice

--- src/main/java/com/splitwise/microservices/expense_service/repository/ChangeLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.splitwise.microservices.expense_service.repository;
-
-import com.splitwise.microservices.expense_service.entity.ChangeLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChangeLogRepository extends JpaRepository<ChangeLog,Long> {
-}
