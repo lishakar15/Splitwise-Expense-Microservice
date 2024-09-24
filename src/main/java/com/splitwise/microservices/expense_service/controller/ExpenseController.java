ba67814d2a1d972fcb9fc3d7980205746813f22f
Lishakar <lishakarj@gmail.com>
2024-09-24 21:21:59 +0530

    Changed DB to MySql

--- src/main/java/com/splitwise/microservices/expense_service/controller/ExpenseController.java
+++ src/main/java/com/splitwise/microservices/expense_service/controller/ExpenseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/expense")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ExpenseController {
     @Autowired
     ExpenseService expenseService;
